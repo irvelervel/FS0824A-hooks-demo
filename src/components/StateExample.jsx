da8d49db9234fcbf34e89c289a11d6c3b4393a35
Stefano Casasola <ste.casasola@gmail.com>
2024-12-03 11:15:32 +0100

    Finishes useState example

--- src/components/StateExample.jsx
+++ src/components/StateExample.jsx
@@ -1,5 +1,6 @@
 import { Button } from 'react-bootstrap'
 import { useState } from 'react'
+import NameComponent from './NameComponent'
 
 // i React Hooks sono delle funzionalità che arricchiscono i componenti a funzione e li rendono paragonabili in termini di logica ai componenti a classe
 
@@ -19,20 +20,59 @@ const StateExample = () => {
   //   const elementoUno = array[0]
   //   const elementoDue = array[1]
 
+  // equivalente di:
+  //   state = {
+  //     counter: 0
+  //   }
+
   const [counter, setCounter] = useState(0)
+  // anche questo counter, come l'oggetto di stato nei componenti a classe,
+  // è READ-ONLY! l'unico modo per aggiornare counter è passare per setCounter:
+  // setCounter(nuovoValore)
+
+  // l'aggiornamento dello stato non può avvenire al di fuori di un event listener!
+  // setCounter(counter + 1) <-- provoca un loop infinito
+
+  const [name, setName] = useState('Mario')
 
   return (
     <div>
       <h2>useState in React</h2>
       <p>Impariamo ad usare le variabili di stato nei componenti a funzione</p>
       <div className="d-flex justify-content-around align-items-center">
-        <Button variant="success" className="px-5 fs-1">
+        <Button
+          variant="success"
+          className="px-5 fs-1"
+          onClick={() => {
+            setCounter(counter - 1)
+          }}
+        >
           -
         </Button>
         <p className="mb-0">{counter}</p>
-        <Button variant="success" className="px-5 fs-1">
-          +
-        </Button>
+        <div>
+          <Button
+            variant="success"
+            className="px-5 fs-1"
+            onClick={() => {
+              setCounter(counter + 1)
+            }}
+          >
+            +
+          </Button>
+          <Button
+            variant="danger"
+            className="px-5 fs-1"
+            onClick={() => {
+              setCounter(0)
+            }}
+          >
+            RESET
+          </Button>
+        </div>
+      </div>
+      <div>
+        <NameComponent nameProp={name} setNameFunction={setName} />
       </div>
     </div>
   )
